6b543145c128052de7480134f783f6382915d150
brunomoraes15 <brunosilvamoraes2006@gmail.com>
2024-10-02 16:49:23 -0300

    Commit Inicial do Projeto SUSNET 🚀🚀🚀

---
+++ main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI, Form, Request
+from fastapi.responses import RedirectResponse
+from fastapi.templating import Jinja2Templates
+import uvicorn
+
+app = FastAPI()
+template = Jinja2Templates(directory="templates")
+
+
+@app.get("/")
+def get_root(request: Request):
+    return template.TemplateResponse("cadastro.html", {"request": request})
+
+@app.get("/cadastro")
+def get_cadastro(request: Request):
+    return template.TemplateResponse("cadastro.html", {"request": request})
+
+
+@app.post("/post_cadastro")
+def post_cadastro(
+    nome:       str = Form(...), 
+    user:      str = Form(...), 
+    email:  str = Form(...), 
+    senha:    str = Form(...), 
+    categoria:  str = Form(...)):
+    return RedirectResponse(url="/", status_code=303)
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=8000, reload=True)
